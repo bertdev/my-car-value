7cd466c9ca37444b37d334edfd2f0f6796c2b219
bertdev <herbertmatos10@hotmail.com>
2022-05-23 16:23:45 -0300

    adiciona uso de repositorio criado pelo typeorm e cria o primeiro metodo para criacao de user

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,9 @@
+import { IsString, IsEmail } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  create(email: string, password: string): Promise<User> {
+    const user = this.usersRepository.create({ email, password });
+
+    return this.usersRepository.save(user);
+  }
+}
