ad01504ebb648637707ca7afd9440d38409ae18b
bertdev <herbertmatos10@hotmail.com>
2022-06-05 12:36:52 -0300

    cria teste para signup de auth service

--- src/users/services/auth.service.spec.ts
+++ src/users/services/auth.service.spec.ts
@@ -27,4 +27,18 @@ describe('AuthService', () => {
   it('can create an instance of AuthService', async () => {
     expect(service).toBeInstanceOf(AuthService);
   });
+
+  it('can create a new user with a salted and hashed password', async () => {
+    const credentials = {
+      email: 'test@test.com',
+      password: 'test1234',
+    };
+    const user = await service.signup(credentials.email, credentials.password);
+    const [salt, hash] = user.password.split('.');
+
+    expect(user.email).toEqual(credentials.email);
+    expect(user.password).not.toEqual(credentials.password);
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
 });
