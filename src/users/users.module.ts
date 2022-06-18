a0f4999d5433474cf80c4f49519420074f35781b
bertdev <herbertmatos10@hotmail.com>
2022-06-18 16:18:35 -0300

    muda o current user de um interceptor para um midleware quando tentamos criar o admin guard para verificar se o current user era um user admin ocorria erro devido a precedencia de execucao de guard e interceptor

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,17 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService, AuthService } from './services';
 import { User } from './user.entity';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from './middlewares/curent-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    AuthService,
-    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
-  ],
+  providers: [UsersService, AuthService],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
